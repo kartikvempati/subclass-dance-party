ec3fed0e6273aeee53ce0be255a7c2f66d4ea94d
kartikvempati <kvempati11@gmail.com>
2015-12-01 11:47:24 -0800

    Added breakDancer.

--- src/breakDancer.js
+++ src/breakDancer.js
@@ -3,10 +3,12 @@
  */
 var breakDancer = function (top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, 1500);
+  this.$node.removeClass('dancer').addClass("breakDancer");
+  this.$node.append('<img src="fluffy.gif" >');
 
   var oldStep = this.step;
 
-  this.step = function() {
+  this.step = function () {
     // call the old version of step at the beginning of any call to this new version of step
     oldStep.call(this);
     // toggle() is a jQuery method to show/hide the <span> tag.
@@ -16,11 +18,22 @@ var breakDancer = function (top, left, timeBetweenSteps) {
     var upPosition = this.$node.position().top;
     var max = 30;
     var min = -30;
-    var degrees = Math.random() * 720 -360;
-    this.$node.animate({
-      '-webkit-transform' : 'rotate('+ degrees +'deg)'
-    });
+    var degrees = Math.random() * 720 - 360;
+    //this.$node.animate({
+    //  '-webkit-transform' : 'rotate('+ degrees +'deg)'
+    //});
+    var rotationDirection = 1;
 
+    if( 0.5 <= Math.random() ) rotationDirection = -1;
+    console.log(rotationDirection * 360);
+    this.$node.animate({borderSpacing: rotationDirection * 360}, {
+      step: function (now, fx) {
+        $(this).css('-webkit-transform', 'rotate(' + now + 'deg)');
+        $(this).css('-moz-transform', 'rotate(' + now + 'deg)');
+        $(this).css('transform', 'rotate(' + now + 'deg)');
+      },
+      duration: 1500
+    }, 'linear');
   };
 };
 
